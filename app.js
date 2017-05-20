7da6c95c6aa546c27741968203d16b37b093a722
UmamaheswararaoMeka <umamahesh_16@yahoo.co.in>
2017-05-20 16:43:49 +0530

    Adds compile link features

--- app.js
+++ app.js
@@ -2,12 +2,40 @@ var app = angular.module("myApp",[])
 
 app.controller("myController", ["$scope", function($scope){
 	$scope.message = "This is message from controller";
+	$scope.company= "Techminds Pvt Ltd"
+	$scope.userDetails = [
+		{name : "Scott Desatnick", address : "Boston, United States"},
+		{name : "Adam Colson", address : " Texas, United States"},
+		{name : "Tuan Bui", address : "Ohio, USA"},
+		{name : "JP Poveda", address : "Colarado, Canada"}
+	]
+
+	$scope.changeCompanyName= function(){
+		$scope.company="Techmind Solutions";
+	}
+
+
 }])
 
 app.directive("myCustomerDetailsDirective", function(){
 	return {
 		restrict : 'EACM',
-		template: "<h1>This is custom directive</h1>",
-		replace: true
-	}
-})
+		templateUrl:'customerDetails.html' ,
+		replace: true,
+		scope: {
+			companyName : '@',
+			userDetails : '=',
+			change : '&'
+		},
+
+		link: function(scope, elem, attrs){
+
+				console.log("In postlink")
+				console.log(scope)
+				console.log(elem)
+				console.log(attrs)		
+				}
+
+			}
+		}
+)
